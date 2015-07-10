3655ec995e9807ae5261b089205efdd935bdc4fc
tht52 <tht52@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-10 17:46:51 +0000

    First version added.
    git-svn-id: https://plugins.svn.wordpress.org/bangla-font-fixer/trunk@1196462 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Bangla Font Fixer
+Plugin URI: http://bhinno.net/Bangla-Font-Fixer
+Description: বাংলা ফন্ট ভেঙে যাওয়ার সমস্যা থেকে পরিত্রাণ পান।
+Author: Tasnimul Hasan Tauhid
+Version: 1.0
+Author URI: http://www.facebook.com/tht52
+*/
+
+//For custom font in wp_head and front page body
+
+add_action('wp_head', 'bffbtht_wp_head_font');
+
+function bffbtht_wp_head_font() {
+  echo '<style>
+    body, td, textarea, input, select {
+      font-family: "SolaimanLipi";
+    } 
+#wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
+      font-family: "SolaimanLipi";
+    } 
+  </style>';
+}
+
+//For custom font in wp_admin
+
+add_action('admin_head', 'bffbtht_admin_font');
+
+function bffbtht_admin_font() {
+  echo '<style>
+    body, td, textarea, input, select {
+      font-family: "SolaimanLipi";
+    } 
+#wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
+      font-family: "SolaimanLipi";
+    } 
+  </style>';
+}
+
+//For custom font in login page
+
+add_action('login_head', 'bffbtht_login_font');
+
+function bffbtht_login_font() {
+  echo '<style>
+p {font-family: "SolaimanLipi";} 
+  </style>';
+}
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Bangla Font Fixer ===
+Contributors: tht52
+Tags: bangla, font, fix, fixer, break down
+Requires at least: 3.0
+Tested up to: 4.2.2
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+বাংলা ফন্ট ভেঙে যাওয়ার সমস্যা থেকে পরিত্রাণ পান।
+
+== Description ==
+
+
+আমরা সকলেই জানি যে বাঙলা ফন্ট যেকোনো ব্রাউজারে ভেঙে যায়। এই সমস্যা থেকে পরিত্রাণ পাওয়ার জন্য আপনাদের জন্য তৈরি করেছি বাংলা ফন্ট ফিক্সার। 
+
+<b>প্লাগিনটি কি ভাবে কাজ করে?</b>
+এই প্লাগিনটি আপনার সাইটের সকল বাংলা লেখাকে সোলাইমানলিপি ফন্ট করে দেবে। উল্লেখ্য যে সোলাইমানলিপি ফন্ট অধিকাংশ উন্নত বাংলা ওয়েবসাইটে ব্যাবহার করা হয়।
+
+== Installation ==
+
+১। "bangla_font_fixer" আপনার সাইটের wp-content ডিরেক্টরিতে আপলোড করুন।
+২।ইন্সটল করা প্লাগিন সমূহ (installed plugins) থেকে Bangla Font Fixer প্লাগিনটি সক্রিয় করুন।
+
+== Frequently Asked Questions ==
+
+= আমি প্লাগিনটি ইন্সটল করেছি তবে ফন্ট এখনো ভেঙে যাচ্ছে =
+
+সোলাইমানলিপি (SolaimanLipi) ফন্টটি ইন্সটল করে নিন। আশা করি সমস্যা সমাধান হয়ে যাবে।
+
+== Changelog ==
+
+= 1.0 =
+* প্রথম প্রকাশ করা হল।
