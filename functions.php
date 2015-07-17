b11953d05a28d2f67f2f388bc2a420a4ebd4802f
tht52 <tht52@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-17 12:25:12 +0000

    More tag support added.
    git-svn-id: https://plugins.svn.wordpress.org/bangla-font-fixer/tags/1.1@1200977 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Bangla Font Fixer
-Plugin URI: http://bhinno.net/bangla-font-fixer
+Plugin URI: http://bff.bhinno.net/
 Description: বাংলা ফন্ট ভেঙে যাওয়ার সমস্যা থেকে পরিত্রাণ পান।
 Author: Tasnimul Hasan Tauhid
-Version: 1.0
+Version: 1.1
 Author URI: http://www.facebook.com/tht52
 */
 
@@ -14,11 +14,11 @@ add_action('wp_head', 'bffbtht_wp_head_font');
 
 function bffbtht_wp_head_font() {
   echo '<style>
-    body, td, textarea, input, select {
-      font-family: "SolaimanLipi";
+    html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video, textarea, input, select {
+      font-family: "SolaimanLipi", Vrinda;
     } 
 #wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
-      font-family: "SolaimanLipi";
+      font-family: "SolaimanLipi", Vrinda;
     } 
   </style>';
 }
@@ -30,10 +30,10 @@ add_action('admin_head', 'bffbtht_admin_font');
 function bffbtht_admin_font() {
   echo '<style>
     body, td, textarea, input, select {
-      font-family: "SolaimanLipi";
+      font-family: "SolaimanLipi", Vrinda;
     } 
 #wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
-      font-family: "SolaimanLipi";
+      font-family: "SolaimanLipi", Vrinda;
     } 
   </style>';
 }
@@ -44,6 +44,6 @@ add_action('login_head', 'bffbtht_login_font');
 
 function bffbtht_login_font() {
   echo '<style>
-p {font-family: "SolaimanLipi";} 
+p {font-family: "SolaimanLipi", Vrinda;} 
   </style>';
 }
